5d6396be30b46ce3890e15b52edea500f9c16541
Gaurav kumar <38521462+kumargaurav05@users.noreply.github.com>
2020-07-26 13:37:15 +0530

    2nd commit

--- Mars Rover (Gaurav)/javascript.js
+++ Mars Rover (Gaurav)/javascript.js
@@ -1,30 +1,71 @@
 var method = "GET";
-var imgDate = "2015-02-02"
-var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/Curiosity/photos?earth_date=" + imgDate + "&camera=FHAZ&api_key=21760btk212aupCr25Qmmc1jK6yVyMc7Fin6bM8A"
 var mode = true;
+var imgDate;
 
+var api_key = "21760btk212aupCr25Qmmc1jK6yVyMc7Fin6bM8A"
+var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=" + api_key + "&earth_date=";
+var date;
+var data;
+
+var urlIndex = 0;
 
 function sendHttpRequest(method, url, mode) {
-    return new Promise((resolve, reject) => {
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function () {
 
-            if (this.readyState == 4 && this.status == 200) {
-                var data = JSON.parse(this.response);
-                resolve(data);
-                console.log(data);
-            } else if (this.status == 404) {
-                reject(this.status);
+    return new Promise((resolve, reject) => {
+
+        var req = new XMLHttpRequest();
+        req.onload = function () {
+            if (req.readyState == 4) {
+                if (req.status == 200) {
+                    data = JSON.parse(req.response);
+                    resolve(data);
+                    console.log(data)
+                }
+                if (req.status == 404) {
+                    console.log("file not found");
+                }
             }
         };
-        req.open(method, url, mode);
+        req.open(method, url + imgDate, mode);
         req.send();
-    });
+    })
 }
 
-sendHttpRequest(method, url, mode).then(function () {
-    console.log("Resolved");
+ function disablebtn(){
+     document.getElementById("next").disabled = true;
+ }
 
-}).catch(function () {
-    console.log("you did very wrong sir !");
+ function enableBtn(){
+    document.getElementById("next").disabled = false;
+ }
+
+function showPic()
+{
+    var image = document.getElementById("pic");
+    image.onload = function(){
+        enableBtn();
+    }
+    disablebtn();
+    image.src = data.photos[urlIndex].img_src;
+}
+function nextPic()
+{
+    urlIndex++;
+    if(urlIndex > data.photos.length)
+    {
+        urlIndex = 0;
+    }
+   showPic();
+}
+
+
+document.addEventListener("change", function () {
+    imgDate = document.getElementById("dateText").value;
+    sendHttpRequest(method, url, mode).then((test) =>
+    {
+        data = test;
+        urlIndex = 0;
+        showPic();
+
+    });
 })
