50fe30838aba6f8c55f16026bf1d70e6cfa6c1a1
Gaurav kumar <38521462+kumargaurav05@users.noreply.github.com>
2020-07-29 22:54:00 +0530

    almost done

--- marsRover/javascript.js
+++ marsRover/javascript.js
@@ -1,37 +1,85 @@
 var method = "GET";
 var mode = true;
 var imgDate;
+var rover;
 var api_key = "21760btk212aupCr25Qmmc1jK6yVyMc7Fin6bM8A"
 var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=" + api_key + "&earth_date=";
 var date;
 var data;
 var front_camera = "&camera=FHAZ";
 var Rear_camera = "&camera=RHAZ";
+var Nav_camera = "&camera=navcam";
+var curiosity = "/curiosity/";
+var opportunity = "/opportunity/";
+var spirit = "/spirit/";
 var showFrontImage;
 var showRearImage;
 
 var urlIndex = 0;
 
-function sendHttpRequest(method, url, mode) {
+var Curiosity_box;
+var Opportunity_box;
+var Spirit_box;
+var update_url;
+
+
+function chooseRover()
+{
+
+    curiosity_box = document.getElementById('Curiosity').checked;
+    Opportunity_box = document.getElementById('Opportunity').checked;
+    Spirit_box = document.getElementById('Spirit').checked;
+
+if(curiosity_box === true)
+{
+    rover = curiosity;
+}
+else if(Opportunity_box === true)
+{
+    rover = opportunity;
+}
+else
+{
+    rover = spirit;
+}
+
+update_url  = "https://api.nasa.gov/mars-photos/api/v1/rovers"+rover+"photos?api_key=" + api_key + "&earth_date=";
+console.log(update_url);
+
+}
+
+
+
+
+
+
+function sendHttpRequest(method, update_url, mode) {
 
     return new Promise((resolve, reject) => {
 
+        // chooseRover();
+        // console.log(rover);
+
         var req = new XMLHttpRequest();
         req.onload = function () {
-            if (req.readyState == 4) {
-                if (req.status == 200) {
+            if (req.readyState == 4 && req.status == 200) {
                     data = JSON.parse(req.response);
                     resolve(data);
                     console.log(data)
-                }
+                
                 if (req.status == 404) {
                     console.log("file not found");
                 }
             }
         };
-        req.open(method, url, mode);
+       // chooseRover();
+        req.open(method, update_url, mode);
         req.send();
     })
+}
+
+function loadingImg(mode) {
+    document.getElementById('loadImg').style.display = mode;
 }
 
 function disablebtn() {
@@ -43,17 +91,41 @@ function enableBtn() {
     document.getElementById("next").disabled = false;
     document.getElementById("prev").disabled = false;
 }
+// This function will leave a message whenever photos will not available.
+function errorImage(mode) {
+    disableImage();
+    document.getElementById('notFound').style.display = mode;
+}
+
+function enableImage() {
+    document.getElementById("pic").style.display = "inline-block";
+}
+//
+function disableImage() {
+    document.getElementById("pic").style.display = "none";
+
+}
 
 function showPic() {
     var image = document.getElementById("pic");
     image.onload = function () {
+        //alert("hello");
+        loadingImg('none');
+        enableImage();
         enableBtn();
         document.getElementById("toggle").style.visibility = "hidden";
-
+    }
+    disablebtn();
+    image.onerror = () => {
+        loadingImg("none");
+        disableImage();
+        errorImage('inline');
     }
 
-    disablebtn();
     image.src = data.photos[urlIndex].img_src;
+
+
+
 }
 
 function nextPic() {
@@ -63,65 +135,99 @@ function nextPic() {
 
     }
     if (urlIndex < data.photos.length) {
+       // disableImage();
+      //  loadingImg("inline");
+
         showPic();
+        console.log(urlIndex);
     }
 }
 
 function prevPic() {
-    try {
-        urlIndex--;
-        console.log(urlIndex);
+    urlIndex--;
+    if (urlIndex < 0) {
+        urlIndex = data.photos.length -1;
+    } else {
         showPic();
-
-    } catch (error) {
-        urlIndex = data.photos.length;
-        console.log("You can't go back!");
-        enableBtn();
-
-
+        console.log(urlIndex);
     }
 }
 
 function date_change() {
 
     imgDate = document.getElementById("dateText").value;
-    sendHttpRequest(method, url + imgDate + front_camera, mode).then((test) => {
-        data = test;
-       // urlIndex = 0;
-       // showFrontImage = showPic();
-    });
-    sendHttpRequest(method, url + imgDate + Rear_camera, mode).then((test) => {
-        data = test;
-       // urlIndex = 0;
-       // showRearImage = showPic();
-    });
 
 }
+
 function fcam() {
     console.log('front camera is enabled!');
-   // date_change();
-    sendHttpRequest(method,url + imgDate +front_camera, mode).then((test) => {
-        data = test;
-        urlIndex = 0;
-        showPic();
+    sendHttpRequest(method, update_url + imgDate + front_camera, mode).then((test) => {
+
+        if (data.photos.length < 1) {
+            // alert("we are here");
+            errorImage('inline');
+            loadingImg('none');
+            // disableImage();
+            console.log("no data found for this date!");
+
+        } else {
+            errorImage('none');
+            data = test;
+            urlIndex = 0;
+            showPic();
+        }
+
     });
 }
 
 function rcam() {
     console.log('Rear camera is enabled!');
-    date_change();
-    sendHttpRequest(method,url + imgDate +Rear_camera, mode).then((test) => {
+    // date_change();
+    sendHttpRequest(method, update_url + imgDate + Rear_camera, mode).then((test) => {
         data = test;
-        urlIndex = 0;
-        showPic();
+
+        if (data.photos.length < 1) {
+            // alert("we are here");
+            errorImage('inline');
+            loadingImg('none');
+            // disableImage();
+            console.log("no data found for this date!");
+
+        } else {
+            errorImage('none');
+            data = test;
+            urlIndex = 0;
+            showPic();
+        }
+    });
+}
+
+function navcam()
+{
+    console.log('Nav cam is enabled!');
+    // date_change();
+    sendHttpRequest(method, update_url + imgDate + Nav_camera, mode).then((test) => {
+        data = test;
+
+        if (data.photos.length < 1) {
+            // alert("we are here");
+            errorImage('inline');
+            loadingImg('none');
+            // disableImage();
+            console.log("no data found for this date!");
+
+        } else {
+            errorImage('none');
+            data = test;
+            urlIndex = 0;
+            showPic();
+        }
     });
 }
 
 document.addEventListener("change", date_change, true);
 // document.addEventListener("click",date_change,true);
 
-function changeEvent(change) {
-    document.getElementById("toggle").style.visibility = "visible";
+function onChangeDate(){
+    
 }
-
-document.addEventListener("change", changeEvent);
